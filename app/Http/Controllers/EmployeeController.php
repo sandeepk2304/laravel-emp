71b3e32d3a121229f64db8f669e78dae425a29e4
Sandeep Khandelwal <sandeep.khandelwal2304@gmail.com>
2019-07-03 21:44:05 +0530

    init

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    //
+    public function index(){
+        return view('employee/index')->with([
+            'pageTitle'=>'List'
+        ]);
+    }
+}
